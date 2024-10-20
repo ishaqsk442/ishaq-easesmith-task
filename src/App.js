b952e8c1e79510e60006e296f0a43c196dc0a76e
ishaq shaik <ishaq@marmeto.com>
2024-10-20 21:02:17 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Navbar from './Navbar/navbar';
 import Home from './Home'
-import SearchProduct from './SearchProducts';
-import ProductsSummery from './ProductsSummery';
-import NurserySplideProducts from './NurserySplideProducts'
 import ThankYouPage from './ThankYouPage';
-
-
-
 
 
 function App() {
@@ -22,17 +15,5 @@ function App() {
     </Router>
   );
 }
-
-// function App() {
-//   return (
-//     <div className='App'>
-      
-//       <Navbar/>
-//       <SearchProduct/>
-//       <ProductsSummery/>
-//       <NurserySplideProducts/>
-//     </div>
-//   );
-// }
 
 export default App;
